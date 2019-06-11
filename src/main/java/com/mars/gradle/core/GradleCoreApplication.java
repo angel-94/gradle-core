3dc4f31f4db285b4e91bfc713d6778e3004879d5
Angel Ruiz <angel138ms@gmail.com>
2019-06-11 10:42:17 -0500

    Initial commit

---
+++ src/main/java/com/mars/gradle/core/GradleCoreApplication.java
@@ -0,0 +1,13 @@
+package com.mars.gradle.core;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GradleCoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GradleCoreApplication.class, args);
+	}
+
+}
